065b7d14f3bac1e70d5715eea55b164dd1c2a1ac
Sunny Prakash <sunny.prakashgm@gmail.com>
2023-10-21 18:33:39 +0200

    initial setup for client

--- libs/dynamoDb-lib.js
+++
@@ -1,9 +0,0 @@
-import AWS from 'aws-sdk';
-
-const client = new AWS.DynamoDB.DocumentClient();
-
-export default {
-    get: params => client.get(params).promise(),
-    put: params => client.put(params).promise(),
-    update: params => client.update(params).promise(),
-};
